5656fdb310a0450cfc53c432a2e8798a27a0cddc
Andrew Shark <ashark@linuxcomp.ru>
2024-02-28 12:00:00 +0300

    Add IPC.Null module

---
+++ ksblib/IPC/Null.py
@@ -0,0 +1,26 @@
+from ..Util.Conditional_Type_Enforced import conditional_type_enforced
+from overrides import override
+
+from .IPC import IPC
+
+
+@conditional_type_enforced
+class IPC_Null(IPC):
+    """
+    Dummy IPC module in case SysVIPC doesn't work or async mode is not needed.
+    """
+    
+    def __init__(self):
+        IPC.__init__(self)
+        self.msgList = []  # List of messages.
+    
+    @override(check_signature=False)
+    def sendMessage(self, msg) -> bool:
+        self.msgList.append(msg)
+        return True
+    
+    @override(check_signature=False)
+    def receiveMessage(self) -> bytes:
+        if not len(self.msgList) > 0:
+            return b""
+        return self.msgList.pop(0)
