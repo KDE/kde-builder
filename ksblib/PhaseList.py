df45f048c96e124a3b4348c810deef75d0a5b75b
Andrew Shark <ashark@linuxcomp.ru>
2024-02-28 12:00:00 +0300

    Add PhaseList module

---
+++ ksblib/PhaseList.py
@@ -0,0 +1,74 @@
+"""
+DESCRIPTION
+
+Handles the "phases" for kdesrc-build, e.g. a simple list of phases, and
+methods to add, clear, or filter out phases. Meant to be assigned to a
+:class:`Module`.
+
+SYNOPSIS
+::
+
+    phases = PhaseList()
+    mod.createBuildSystem() if phases.has('buildsystem')
+    phases.filterOutPhase('update') if ctx.getOption('build-only')
+"""
+
+from .Util.Conditional_Type_Enforced import conditional_type_enforced
+
+
+@conditional_type_enforced
+class PhaseList:
+    def __init__(self, phases: list | None = None):
+        """
+        Constructs a new phase list, with the provided list of phases or
+        a default set if none are provided.
+        ::
+        
+            phases1 = PhaseList() # default phases
+            print("phases are " + phases1.phaselist.join(', '))
+        
+            phases2 = PhaseList(["update", "test", "install"])
+        """
+        if not phases:
+            phases = ["update", "build", "install"]
+        self.phaselist = phases
+    
+    def filterOutPhase(self, phase: str) -> None:
+        """
+        Removes the given phase from the list, if present.
+        """
+        self.phaselist = [item for item in self.phaselist if item != phase]
+    
+    def addPhase(self, phase: str) -> None:
+        """
+        Adds the given phase to the phase list at the end.
+        
+        This is probably a misfeature; use insert at index to add the phase
+        in the right spot if it's not at the end.
+        """
+        if not self.has(phase):
+            self.phaselist.append(phase)
+    
+    def has(self, phase: str) -> bool:
+        """
+        Returns true if the given phase is in the phase list.
+        """
+        return any(element == phase for element in self.phaselist)
+    
+    def phases(self, args: list | None = None) -> list:
+        """
+        If provided a list, clears the existing list of phases and
+        resets them to the provided list.
+        If not provided a list, returns the list of
+        phases without modifying the instance.
+        """
+        assert args is None or isinstance(args, list)
+        if args:
+            self.phaselist = args
+        return self.phaselist
+    
+    def clear(self) -> None:
+        """
+        Empties the phase list.
+        """
+        self.phaselist = []
