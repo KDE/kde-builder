a188c1d3412adc1666c25456de12096b3efd9974
Andrew Shark <ashark@linuxcomp.ru>
2024-10-18 01:28:30 +0300

    Add meson-options option

--- kde_builder_lib/build_system/meson.py
+++ kde_builder_lib/build_system/meson.py
@@ -14,7 +14,7 @@ class BuildSystemMeson(BuildSystem):
     Note that Meson requires Ninja as its underlying build system so anything dealing with Meson
     can assume Ninja support is present.
 
-    Control the flags passed to Meson's setup step using the ``configure-flags`` option.
+    Control the flags passed to Meson's setup step using the ``meson-options`` option.
     """
 
     @staticmethod
@@ -29,9 +29,7 @@ class BuildSystemMeson(BuildSystem):
         builddir = module.fullpath("build")
         installdir = module.installation_path()
 
-        # "module"-limited option grabbing can return None, so use Logical Defined-Or
-        # to convert to empty string in that case.
-        setup_options = Util.split_quoted_on_whitespace(module.get_option("configure-flags", "module") or "")
+        setup_options = Util.split_quoted_on_whitespace(module.get_option("meson-options"))
 
         exitcode = Util.run_logged(module, "meson-setup", sourcedir, ["meson", "setup", builddir, "--prefix", installdir, *setup_options])
         return Util.good_exitcode(exitcode)
