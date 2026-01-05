07b6b8f060c526886df8c8ca4c928c86a6eebb60
Andrew Shark <ashark@linuxcomp.ru>
2026-01-05 22:18:54 +0300

    refactor: Extract test metadata.yaml to separate files

--- tests/integration/test_repository-resolving.py
+++ tests/integration/test_repository-resolving.py
@@ -31,8 +31,8 @@ def test_repository_resolution():
     assert module_list[10].name == "gnome-binaryeditor", "Right order: third party project (ordered as placed in config)"
     assert module_list[11].name == "elisa", "Right order: kde project expanded from group (ordered as placed in config)"
 
-    assert module_list[0].get_option("#resolved-repository") == "kde:kcalc.git", "First party project, and explicitly using \"kde-projects\" magic value"
-    assert module_list[1].get_option("#resolved-repository") == "kde:juk.git", "First party project, and implicitly using \"kde-projects\" magic value"
+    assert module_list[0].get_option("#resolved-repository") == "kde:test/kcalc.git", "First party project, and explicitly using \"kde-projects\" magic value"
+    assert module_list[1].get_option("#resolved-repository") == "kde:test/juk.git", "First party project, and implicitly using \"kde-projects\" magic value"
     assert module_list[2].get_option("#resolved-repository") == "https://github.com/torvalds/konsole", "First party project, but using one of defined git-repository-bases"
     assert module_list[3].get_option("#resolved-repository") == "", "First party project, but set empty repository"
     assert module_list[4].get_option("#resolved-repository") == "https://example.com/kde-builder-fork", "First party project, and set to non-empty, but not magic, and not from git-repository-base"
@@ -42,6 +42,6 @@ def test_repository_resolution():
     assert module_list[8].get_option("#resolved-repository") == "", "Third party project, but set empty repository"
     assert module_list[9].get_option("#resolved-repository") == "https://example.com/gnome-text-editor", "Third party project, and set to non-empty, but not magic and not from git-repository-base - assumed full url"
     assert module_list[10].get_option("#resolved-repository") == "gnome-gh", "Third party project, and set to non-empty, but not magic and not from git-repository-base - assumed repository base, but not defined"
-    assert module_list[11].get_option("#resolved-repository") == "kde:elisa.git", "Group implicitly using \"kde-projects\" magic value, which is inherited by projects expanded from it"
+    assert module_list[11].get_option("#resolved-repository") == "kde:test/elisa.git", "Group implicitly using \"kde-projects\" magic value, which is inherited by projects expanded from it"
 
     Debug().set_pretending(False)  # disable pretending, to not influence on other tests, because Debug is singleton
